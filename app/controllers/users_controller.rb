18cace8787d48f502d71c08dbc89e91a4a93b2e0
kkterai <kkterai@gmail.com>
2016-11-03 14:01:36 -0700

    build out login, signup, and logout routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,47 @@
+require 'rack-flash'
+
 class UsersController < ApplicationController
+  use Rack::Flash
 
   get '/login' do
+    if logged_in?
+      redirect '/list'
+    else
+      erb :'users/login'
+    end
+  end
 
-    erb :'users/login'
+  post "/login" do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/list"
+    else
+      redirect "/login"
+    end
   end
 
   get '/signup' do
+    if logged_in?
+      redirect '/list'
+    else
+      erb :'users/create_user'
+    end
+  end
 
-    erb :'users/create_user'
+  post '/signup' do
+    user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    if user.save
+      session[:user_id] = user.id
+        redirect "/tweets"
+    elsif !logged_in?
+        redirect '/signup'
+    end
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/login'
   end
 
 end
